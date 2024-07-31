33eccd5a79e1d8eb7a6738df1ee56c68a7e4470d
Matthew Hood <67388369+pumhood@users.noreply.github.com>
2024-07-31 12:34:58 -0400

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# shellcheck disable=SC2034
+BUILDER_IMAGE=""
+BUILDER_VERSION=""
+BASE_IMAGE="php"
+BASE_VERSION="8-apache"
+IMAGE_NAME="librespeed"
+IMAGE_VERSION="1.0"
+BUILD_ARGS=""
